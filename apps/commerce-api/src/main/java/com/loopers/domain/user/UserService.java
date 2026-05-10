42e68b12aec4902f96294f63f238c9538ef9e3a3
minuuu0 <rlamw2000@naver.com>
2026-05-10 21:15:46 +0900

    feat: userRepository 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserService.java
@@ -0,0 +1,28 @@
+package com.loopers.domain.user;
+
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Component
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public User register(LoginId loginId, Name name, Birth birth, Email email, String rawPassword) {
+        if (userRepository.existsByLoginId(loginId)) {
+            throw new CoreException(ErrorType.CONFLICT, "이미 가입된 로그인 ID 입니다.");
+        }
+
+        Password password = Password.of(rawPassword, birth);
+        String encodedPassword = passwordEncoder.encode(password.value());
+
+        User user = new User(loginId, name, birth, email, encodedPassword);
+        return userRepository.save(user);
+    }
+}
