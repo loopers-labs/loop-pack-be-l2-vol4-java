b98f97119bcb783604cc7d9c84ff02a5e07319b4
byuns <bsj0887@daum.net>
2026-05-12 12:43:24 +0900

    test: 회원가입 통합 테스트 코드 작성

---
+++ apps/commerce-api/src/test/java/com/loopers/domain/user/UserServiceIntegrationTest.java
@@ -0,0 +1,69 @@
+package com.loopers.domain.user;
+
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import com.loopers.utils.DatabaseCleanUp;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+class UserServiceIntegrationTest {
+
+    @Autowired
+    private UserService userService;
+
+    @SpyBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private DatabaseCleanUp databaseCleanUp;
+
+    @AfterEach
+    void tearDown() {
+        databaseCleanUp.truncateAllTables();
+    }
+
+    @DisplayName("회원 가입을 할 때,")
+    @Nested
+    class SignUp {
+
+        @DisplayName("정상적인 요청이면, User 저장이 수행된다.")
+        @Test
+        void savesUser_whenSignUpIsRequested() {
+            // arrange
+            UserModel user = new UserModel("user1", "Pass123!", "홍길동", "test@example.com", "2000-01-01", Gender.MALE);
+
+            // act
+            userService.signUp(user);
+
+            // assert
+            verify(userRepository).save(any(UserModel.class));
+        }
+
+        @DisplayName("이미 가입된 ID로 회원가입 시도 시, 실패한다.")
+        @Test
+        void throwsException_whenLoginIdAlreadyExists() {
+            // arrange
+            UserModel user = new UserModel("user1", "Pass123!", "홍길동", "test@example.com", "2000-01-01", Gender.MALE);
+            userService.signUp(user);
+
+            UserModel duplicate = new UserModel("user1", "Pass456@", "김길동", "other@example.com", "1990-05-15", Gender.FEMALE);
+
+            // act
+            CoreException exception = assertThrows(CoreException.class, () -> userService.signUp(duplicate));
+
+            // assert
+            assertThat(exception.getErrorType()).isEqualTo(ErrorType.CONFLICT);
+        }
+    }
+}
