89e4c62ccd5561b59ae136edad98380e0e67e4a7
eelseungmin <eelseungmin@gmail.com>
2026-05-13 22:44:11 +0900

    feat: 회원 엔티티 및 인프라스트럭처 레포지토리 구현
    - Member JPA 엔티티 구현 및 BaseEntity 상속 (Soft Delete 적용)
    - Spring Data JPA를 활용한 MemberJpaRepository 추가
    - 도메인 레포지토리 인터페이스의 구현체인 MemberRepositoryImpl 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/member/MemberRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.loopers.infrastructure.member;
+
+import com.loopers.domain.member.Member;
+import com.loopers.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepositoryImpl implements MemberRepository {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @Override
+    public boolean existsByLoginId(String loginId) {
+        return memberJpaRepository.existsByLoginId(loginId);
+    }
+
+    @Override
+    public void save(Member member) {
+        memberJpaRepository.save(member);
+    }
+}
