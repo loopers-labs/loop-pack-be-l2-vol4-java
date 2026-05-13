dab5437d369ff2df3cea9ed01dd4c929b292fdd1
kylekim2123 <kyle2123@naver.com>
2026-05-14 00:16:58 +0900

    refactor: WebMvcConfig을 support.config로 이동
    [structural]
    - 위치만 변경, 동작 변화 없음
    - interfaces.api.auth는 인증 도메인 어휘에 한정되도록 정리하고, MVC 횡단 설정인 WebMvcConfig는 support.config로 이동 (support.error와 동등한 cross-cutting 위치)

--- apps/commerce-api/src/main/java/com/loopers/support/config/WebMvcConfig.java
+++ apps/commerce-api/src/main/java/com/loopers/support/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
-package com.loopers.interfaces.api.auth;
+package com.loopers.support.config;
 
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.loopers.interfaces.api.auth.AuthenticatedUserArgumentResolver;
 
 import lombok.RequiredArgsConstructor;
 
