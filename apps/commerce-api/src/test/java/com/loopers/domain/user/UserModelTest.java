299454760bd8c4f0dee9a0e61bbe76a1a0b876fb
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-05-13 13:50:37 +0900

    test: userModelTest errorCase 구현

--- apps/commerce-api/src/test/java/com/loopers/domain/user/UserModelTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/user/UserModelTest.java
@@ -1,9 +1,15 @@
 package com.loopers.domain.user;
 
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserModelTest {
     @DisplayName("유저 모델을 생성하는 테스트")
@@ -17,19 +23,57 @@ public class UserModelTest {
             String id = "usertest123";
             String name = "홍길동";
             String password = "abc123!@#";
-            String birthDate = "19950610";
+            LocalDate birthDate = LocalDate.of(1995, 6, 10);
             String email = "test@naver.com";
 
             // act = when
             UserModel userModel = new UserModel(id, password, name, birthDate, email);
 
             // assert = then
-            Assertions.assertAll(
-                () -> Assertions.assertEquals(id, userModel.getUserId()),
-                () -> Assertions.assertEquals(name, userModel.getName()),
-                () -> Assertions.assertEquals(birthDate, userModel.getBirthDate()),
-                () -> Assertions.assertEquals(email, userModel.getEmail())
+            assertAll(
+                () -> assertEquals(id, userModel.getUserId()),
+                () -> assertEquals(name, userModel.getName()),
+                () -> assertEquals(birthDate, userModel.getBirthDate()),
+                () -> assertEquals(email, userModel.getEmail())
             );
+        }
+
+        @DisplayName("이름이 없는 경우, 예외가 발생한다.")
+        @Test
+        void throwsException_whenNameIsMissing() {
+            // arrange = given
+            String id = "usertest123";
+            String name = "";
+            String password = "abc123!@#";
+            LocalDate birthDate = LocalDate.of(1995, 6, 10);
+            String email = "test@naver.com";
+
+            // act = when
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new UserModel(id, password, name, birthDate, email);
+            });
+
+            // assert = then
+            assertEquals(ErrorType.BAD_REQUEST, result.getErrorType());
+        }
+
+        @DisplayName("이메일 형식이 올바르지 않은 경우, 예외가 발생한다.")
+        @Test
+        void throwsException_whenEmailIsWrong() {
+            // arrange = given
+            String id = "usertest123";
+            String name = "홍길동";
+            String password = "abc123!@#";
+            LocalDate birthDate = LocalDate.of(1995, 6, 10);
+            String email = "abdfsd";
+
+            // act = when
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new UserModel(id, password, name, birthDate, email);
+            });
+
+            // assert = then
+            assertEquals(ErrorType.BAD_REQUEST, result.getErrorType());
         }
     }
 }
