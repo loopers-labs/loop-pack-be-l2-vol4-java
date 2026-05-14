8bd4480441e9843f5d90681f755c01939b5e6d5c
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-05-15 00:34:20 +0900

    feat: userFacade 구현

--- apps/commerce-api/src/main/java/com/loopers/application/user/UserFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/user/UserFacade.java
@@ -1,4 +1,27 @@
 package com.loopers.application.user;
 
+import com.loopers.domain.user.UserModel;
+import com.loopers.domain.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+@Component
 public class UserFacade {
+    private final UserService userService;
+
+    public void signup(String userId, String password, String name, LocalDate birthDate, String email) {
+        userService.signup(userId, password, name, birthDate, email);
+    }
+
+    public UserInfo getUser(String userId, String password) {
+        UserModel user = userService.getUser(userId, password);
+        return UserInfo.from(user);
+    }
+
+    public void changePassword(String userId, String currentPassword, String newPassword) {
+        userService.changePassword(userId, currentPassword, newPassword);
+    }
 }
