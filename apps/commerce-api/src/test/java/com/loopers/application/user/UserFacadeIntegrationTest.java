bfeb9696347988716e0adbdfff87ee9d35e16ca7
www-author <dream970215@naver.com>
2026-05-15 03:03:38 +0900

    test: 회원 가입 통합 테스트

---
+++ apps/commerce-api/src/test/java/com/loopers/application/user/UserFacadeIntegrationTest.java
@@ -0,0 +1,79 @@
+package com.loopers.application.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+import com.loopers.domain.user.User;
+import com.loopers.domain.user.UserRepository;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import com.loopers.utils.DatabaseCleanUp;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+
+@SpringBootTest
+class UserFacadeIntegrationTest {
+
+  @Autowired private UserFacade userFacade;
+
+  @Autowired private DatabaseCleanUp databaseCleanUp;
+
+  @MockitoSpyBean private UserRepository userRepository;
+
+  @AfterEach
+  void tearDown() {
+    databaseCleanUp.truncateAllTables();
+  }
+
+  @DisplayName("회원 가입을 할 때,")
+  @Nested
+  class SignUp {
+
+    @DisplayName("필요 정보가 모두 유효하면 User 저장이 수행된다.")
+    @Test
+    void savesUser_whenRequiredFieldsAreValid() {
+      // arrange
+      SignUpCommand command =
+          new SignUpCommand("loopers01", "Password1!", "홍길동", "1995-05-15", "loopers@example.com");
+
+      // act
+      UserInfo result = userFacade.signUp(command);
+
+      // assert
+      assertAll(
+          () -> verify(userRepository).save(any(User.class)),
+          () -> assertThat(result.id()).isNotNull(),
+          () -> assertThat(result.loginId()).isEqualTo(command.loginId()),
+          () -> assertThat(result.name()).isEqualTo(command.name()),
+          () -> assertThat(result.birthDate()).isEqualTo(command.birthDate()),
+          () -> assertThat(result.email()).isEqualTo(command.email()));
+    }
+
+    @DisplayName("이미 가입된 ID 로 회원가입 시도 시 실패한다.")
+    @Test
+    void throwsConflictException_whenLoginIdAlreadyExists() {
+      // arrange
+      SignUpCommand command =
+          new SignUpCommand("loopers01", "Password1!", "홍길동", "1995-05-15", "loopers@example.com");
+      userFacade.signUp(command);
+
+      SignUpCommand duplicateCommand =
+          new SignUpCommand("loopers01", "Password1!", "김철수", "1997-07-20", "other@example.com");
+
+      // act
+      CoreException exception =
+          assertThrows(CoreException.class, () -> userFacade.signUp(duplicateCommand));
+
+      // assert
+      assertThat(exception.getErrorType()).isEqualTo(ErrorType.CONFLICT);
+    }
+  }
+}
