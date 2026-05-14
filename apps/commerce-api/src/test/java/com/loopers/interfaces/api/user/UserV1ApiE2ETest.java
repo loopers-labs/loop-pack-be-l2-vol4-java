f1ca02eb66dd482cab6b668b8073063c9a01b942
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-05-14 23:20:45 +0900

    test: UserV1ApiE2ETest signup happyCase 작성

--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserV1ApiE2ETest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserV1ApiE2ETest.java
@@ -1,20 +1,32 @@
 package com.loopers.interfaces.api.user;
 
 import com.loopers.infrastructure.user.UserJpaRepository;
+import com.loopers.interfaces.api.ApiResponse;
 import com.loopers.utils.DatabaseCleanUp;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserV1ApiE2ETest {
     private final TestRestTemplate testRestTemplate;
     private final UserJpaRepository userJpaRepository;
     private final DatabaseCleanUp databaseCleanUp;
+
+    private static final String DEFAULT_USER_ID = "usertest123";
+    private static final String DEFAULT_PASSWORD = "abc123!@#";
+    private static final String DEFAULT_NAME = "홍길동";
+    private static final LocalDate DEFAULT_BIRTH_DATE = LocalDate.of(1995, 6, 10);
+    private static final String DEFAULT_EMAIL = "test@naver.com";
 
     @Autowired
     public UserV1ApiE2ETest(
@@ -27,16 +39,30 @@ public class UserV1ApiE2ETest {
         this.databaseCleanUp = databaseCleanUp;
     }
 
-    // @AfterAll
-    // void tearDown() {
-    //     databaseCleanUp.truncateAllTables();
-    // }
+    @AfterEach
+    void tearDown() {
+        databaseCleanUp.truncateAllTables();
+    }
 
     // 회원가입
     @DisplayName("POST /api/v1/users")
     @Nested
     class Signup {
+        String requestUrl = "/api/v1/users";
 
+        @DisplayName("유효한 회원가입 정보를 주면, 회원가입이 성공한다.")
+        @Test
+        void successSignup_whenValidSignupInfoIsProvided() {
+            // arrange
+            UserV1Dto.SignupRequest signupRequest = new UserV1Dto.SignupRequest(DEFAULT_USER_ID, DEFAULT_PASSWORD, DEFAULT_NAME, DEFAULT_BIRTH_DATE, DEFAULT_EMAIL);
+
+            // act
+            ParameterizedTypeReference<ApiResponse<Void>> responseType = new ParameterizedTypeReference<ApiResponse<Void>>() {};
+            ResponseEntity<ApiResponse<Void>> response = testRestTemplate.exchange(requestUrl, HttpMethod.POST, new HttpEntity<>(signupRequest), responseType);
+
+            // assert
+            Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        }
     }
 
     // 내 정보 조회
