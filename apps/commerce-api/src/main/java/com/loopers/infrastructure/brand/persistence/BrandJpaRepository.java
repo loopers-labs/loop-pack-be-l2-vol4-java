efa0752161ce0921cb34f591cefef2694b059fa4
GOOHAESEUNG <amu6675@naver.com>
2026-05-25 21:27:18 +0900

    feat: Brand infrastructure 레이어 추가 (JpaRepository, RepositoryImpl)

---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/brand/persistence/BrandJpaRepository.java
@@ -0,0 +1,7 @@
+package com.loopers.infrastructure.brand.persistence;
+
+import com.loopers.domain.brand.model.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandJpaRepository extends JpaRepository<Brand, Long> {
+}
