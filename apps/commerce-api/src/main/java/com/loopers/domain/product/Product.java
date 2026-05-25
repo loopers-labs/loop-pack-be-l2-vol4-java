64564397260189e93a9de23a8dd049c466f3ae58
김성호 <tigertjdgh@gmail.com>
2026-05-25 15:47:47 +0900

    refactor: 상품명을 VO로 분리

--- apps/commerce-api/src/main/java/com/loopers/domain/product/Product.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/Product.java
@@ -1,6 +1,7 @@
 package com.loopers.domain.product;
 
 import com.loopers.domain.BaseEntity;
+import com.loopers.domain.product.vo.ProductName;
 import com.loopers.domain.product.vo.ProductPrice;
 import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
@@ -20,14 +21,18 @@ import lombok.NoArgsConstructor;
 public class Product extends BaseEntity {
 
     private Long brandId;
-    private String name;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "name", nullable = false))
+    private ProductName name;
+
     private String description;
 
     @Embedded
     @AttributeOverride(name = "value", column = @Column(name = "price", nullable = false))
     private ProductPrice price;
 
-    private Product(Long brandId, String name, String description, ProductPrice price) {
+    private Product(Long brandId, ProductName name, String description, ProductPrice price) {
         this.brandId = brandId;
         this.name = name;
         this.description = description;
@@ -38,24 +43,25 @@ public class Product extends BaseEntity {
         if (brandId == null) {
             throw new CoreException(ErrorType.BAD_REQUEST, "브랜드 ID는 비어있을 수 없습니다.");
         }
-        validateInfo(name, description);
-        return new Product(brandId, name, description, ProductPrice.of(price));
+        validateInfo(description);
+        return new Product(brandId, ProductName.of(name), description, ProductPrice.of(price));
     }
 
     public void update(String name, String description, long price) {
-        validateInfo(name, description);
-        this.name = name;
+        validateInfo(description);
+        this.name = ProductName.of(name);
         this.description = description;
         this.price = ProductPrice.of(price);
     }
 
-    private static void validateInfo(String name, String description) {
-        if (name == null || name.isBlank()) {
-            throw new CoreException(ErrorType.BAD_REQUEST, "상품명은 비어있을 수 없습니다.");
-        }
+    private static void validateInfo(String description) {
         if (description == null || description.isBlank()) {
             throw new CoreException(ErrorType.BAD_REQUEST, "상품 설명은 비어있을 수 없습니다.");
         }
+    }
+
+    public String getName() {
+        return name.value();
     }
 
     public long getPrice() {
