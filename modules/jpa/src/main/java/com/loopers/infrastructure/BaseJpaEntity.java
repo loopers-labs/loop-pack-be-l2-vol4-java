1328a5b4cd5299287c0f2532cfd3e19360b7696f
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-05-28 14:04:02 +0900

    feat: BaseJpaEntity 추가

---
+++ modules/jpa/src/main/java/com/loopers/infrastructure/BaseJpaEntity.java
@@ -0,0 +1,55 @@
+package com.loopers.infrastructure;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import java.time.ZonedDateTime;
+
+/**
+ * 생성/수정/삭제 정보를 자동으로 관리해준다.
+ * 재사용성을 위해 이 외의 컬럼이나 동작은 추가하지 않는다.
+ */
+@MappedSuperclass
+@Getter
+public abstract class BaseJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private final Long id = 0L;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private ZonedDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private ZonedDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private ZonedDateTime deletedAt;
+
+    /**
+     * 엔티티의 유효성을 검증한다.
+     * 이 메소드는 PrePersist 및 PreUpdate 시점에 호출된다.
+     */
+    protected void guard() {}
+
+    @PrePersist
+    private void prePersist() {
+        guard();
+
+        ZonedDateTime now = ZonedDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        guard();
+
+        this.updatedAt = ZonedDateTime.now();
+    }
+}
