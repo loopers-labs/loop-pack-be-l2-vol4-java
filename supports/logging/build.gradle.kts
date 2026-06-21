73d0b0199c525fb950d7aea8cc2e76a782858cef
ji-eun-k <jeun313@gmail.com>
2026-06-22 00:42:51 +0900

    chore: 모니터링 툴 추가

--- supports/logging/build.gradle.kts
+++ supports/logging/build.gradle.kts
@@ -3,7 +3,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     // monitoring
     implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
     // Slack Appender
     implementation("com.github.maricn:logback-slack-appender:${project.properties["slackAppenderVersion"]}")
 }
